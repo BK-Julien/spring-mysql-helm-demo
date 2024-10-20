5f47bcd7c4b382457d1ac95958311422580bddb2
dev-bkjulien <optimiste.hazel@gmail.com>
2024-10-20 08:29:21 +0000

    Feature new : mise en place de l'environnement et des dépendances

--- src/main/java/bkj/springmysqlhelmdemo/springmysqlhelmdemo/SpringMysqlHelmDemoApplication.java
+++ src/main/java/bkj/springmysqlhelmdemo/springmysqlhelmdemo/SpringMysqlHelmDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMysqlHelmDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMysqlHelmDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMysqlHelmDemoApplication.class, args);
+    }
 
 }
